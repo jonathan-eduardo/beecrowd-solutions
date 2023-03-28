0f4acff1a89bf02408c84ec680e7d00f20202ffb
Jonathan Silva <jonathanedsilva@gmail.com>
2023-03-28 15:01:08 -0300

    Add new exercise solution

---
+++ solutions/beginner/javascript/1059.js
@@ -0,0 +1,10 @@
+/**
+ * beecrowd problem link:
+ * https:www.beecrowd.com.br/judge/pt/problems/view/1059
+ *
+ * OBS: an account is needed to view the problem
+ */
+
+for (let even = 2; even <= 100; even += 2) {
+  console.log(even)
+}
