e3f472c83beefdd92ba0f59e9fb0c2cad93204fb
Jonathan Silva <jonathanedsilva@gmail.com>
2023-04-25 10:54:46 -0300

    Add new exercise solution

---
+++ solutions/beginner/javascript/1095.js
@@ -0,0 +1,14 @@
+/**
+ * beecrowd problem link:
+ * https:www.beecrowd.com.br/judge/pt/problems/view/1095
+ *
+ * OBS: an account is needed to view the problem
+ *
+ * This problem has no input.
+ */
+
+let i = 1
+for (let j = 60; j >= 0; j -= 5) {
+  console.log(`I=${i} J=${j}`)
+  i += 3
+}
