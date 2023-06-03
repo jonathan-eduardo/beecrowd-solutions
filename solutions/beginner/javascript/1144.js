338e3c3380e0c2b7da82af624edd5a81a1c9d600
Jonathan Silva <jonathanedsilva@gmail.com>
2023-06-03 18:15:47 -0300

    Add new exercise solution

---
+++ solutions/beginner/javascript/1144.js
@@ -0,0 +1,15 @@
+/**
+ * beecrowd problem link:
+ * https:www.beecrowd.com.br/judge/pt/problems/view/1144
+ *
+ * OBS: an account is needed to view the problem
+ *
+ * beecrowd input should be:
+ * const input = +require('fs').readFileSync('/dev/stdin', 'utf8')
+ */
+
+const input = +require('fs').readFileSync('./input.txt', 'utf8')
+for (let i = 1; i <= input; i++) {
+  console.log(i, i ** 2, i ** 3)
+  console.log(i, i ** 2 + 1, i ** 3 + 1)
+}
